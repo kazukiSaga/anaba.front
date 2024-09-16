abab1df4dca03fabfbf94e83c2ac95939ad9ea1f
kazukiSaga <kazukikk1351@gmail.com>
2024-09-17 03:37:01 +0900

    投稿一覧

---
+++ src/views/PaginationModule.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-pagination
+    v-model="pagination.current"
+    :length="pagination.pages"
+    :total-visible="10"
+    density="comfortable"
+    @update:modelValue="emit('get-page', pagination.current)"
+  />
+</template>
+
+<script setup>
+import { defineEmits } from 'vue'
+
+const props = defineProps({
+  pagination: {
+    type: Object,
+    required: true,
+    default: {}
+  }
+})
+
+const emit = defineEmits(['get-page'])
+</script>
+
+<style lang="scss" scoped></style>
