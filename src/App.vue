ffcf4a9c2a8980cc9393cd3b9d535d7b1a14de26
kazukiSaga <kazukikk1351@gmail.com>
2024-09-17 03:35:21 +0900

    appの作成

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
 <template>
   <!-- ルートごとのコンテンツを表示 -->
-  <RouterView />
+  <div id="app">
+    <main-header />
+
+    <RouterView />
+  </div>
 </template>
 
 <script setup>
+import MainHeader from './views/MainHeader.vue'
 import { RouterView } from 'vue-router'
+import 'normalize.css'
 </script>
+
+<style scoped>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+
+#app {
+  margin: 0;
+  padding: 0;
+  width: 1200px;
+  height: 1000px;
+}
+</style>
