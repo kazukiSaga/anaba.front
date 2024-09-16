4bbc70b25982c8c47f6e7cddc64a48bed7db7302
kazukiSaga <kazukikk1351@gmail.com>
2024-09-17 04:56:36 +0900

    url

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const apiUrl = import.meta.env.VITE_API_URL
+const apiUrl = 'https://anaba.onrender.com'
 console.log(apiUrl)
 const instance = axios.create({
   baseURL: apiUrl
