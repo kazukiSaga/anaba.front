f90fb21a0025562793cf4c321468866814706352
kazukiSaga <kazukikk1351@gmail.com>
2024-09-17 03:37:01 +0900

    投稿一覧

--- src/views/SpotIndexPage.vue
+++ src/views/SpotIndexPage.vue
@@ -1,20 +1,162 @@
 <template>
-  <div>
-    <main-header />
-    <h1>投稿一覧</h1>
+  <div width="1200px">
+    <v-text-field
+      :label="'スポット検索'"
+      v-model="q.name_cont"
+      @input="searchSpots"
+      hint="スポット名を入力してください"
+      persistent-hint
+      variant="outlined"
+      density="compact"
+    />
+    <v-select
+      :label="'都道府県検索'"
+      class="mt-2"
+      v-model="q.prefecture_id_eq"
+      hint="都道府県を選択してください"
+      persistent-hint
+      variant="outlined"
+      density="compact"
+      item-title="name"
+      item-value="id"
+      :items="prefectures"
+      @update:modelValue="searchSpots"
+    />
+
+    <v-row justify="center" class="mt-4">
+      <v-card
+        v-for="(spot, index) in spots"
+        :key="index"
+        elevation="16"
+        min-width="200"
+        width="300px"
+        height="auto"
+        class="my-2 mr-2"
+      >
+        <v-card-item>
+          <v-card-title class="mt-2 mb-4 text-body-3"> 投稿一覧</v-card-title>
+          <v-text-field
+            :label="'タイトル'"
+            v-model="spot.name"
+            variant="outlined"
+            density="compact"
+            readonly
+          />
+          <v-textarea
+            :label="'説明'"
+            readonly
+            v-model="spot.body"
+            variant="outlined"
+            density="compact"
+          />
+          <v-select
+            :label="'都道府県'"
+            v-model="spot.prefecture_id"
+            variant="outlined"
+            density="compact"
+            item-title="name"
+            item-value="id"
+            :items="prefectures"
+            readonly
+          />
+          <v-text-field
+            v-model="spot.city"
+            :label="'市町村・番地'"
+            variant="outlined"
+            density="compact"
+            readonly
+          />
+          <!-- <label for="spot-tags">タグ</label> -->
+          <!-- <input type="text" id="spot-tags" placeholder="タグを入力 (カンマ区切り)" /> -->
+        </v-card-item>
+      </v-card>
+      <PaginationModule :pagination="pagination" @get-page="getPage($event)" />
+    </v-row>
   </div>
 </template>
 
 <script setup>
- // import { ref, computed } from 'vue'
-</script>
+import { ref, computed, onMounted } from 'vue'
+import axios from '../plugins/axios'
+import Qs from 'qs'
+import PaginationModule from './PaginationModule.vue'
 
-<script>
-import MainHeader from './MainHeader.vue'
+onMounted(() => {
+  fetchSpots(1)
+})
 
-export default {
-  components: {
-    MainHeader
+const spots = ref([])
+const prefectures = ref([])
+const q = ref({
+  name_cont: null,
+  prefecture_id_eq: null
+})
+const pagination = ref({
+  count: null,
+  current: 1,
+  limit_value: null,
+  next: null,
+  pages: null,
+  previous: null
+})
+
+const getPage = (page) => {
+  fetchSpots(page)
+}
+
+const fetchSpots = (page) => {
+  const token = localStorage.getItem('access-token')
+  const client = localStorage.getItem('client')
+  const uid = localStorage.getItem('uid')
+  const headers = {
+    'access-token': token,
+    uid: uid,
+    client: client
   }
+  const params = {
+    page: page
+  }
+  axios
+    .get('api/v1/spots', { params, headers })
+    .then((res) => {
+      spots.value = res.data.spots
+      pagination.value = res.data.pagination
+      prefectures.value = res.data.prefectures
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+}
+
+const searchSpots = () => {
+  const token = localStorage.getItem('access-token')
+  const client = localStorage.getItem('client')
+  const uid = localStorage.getItem('uid')
+  const headers = {
+    'access-token': token,
+    uid: uid,
+    client: client
+  }
+  const params = {
+    q: q.value
+  }
+
+  axios
+    .get('api/v1/search_spots', {
+      params,
+      headers,
+      headers,
+      paramsSerializer: function (params) {
+        return Qs.stringify(params)
+      }
+    })
+    .then((res) => {
+      spots.value = res.data.spots
+    })
+    .catch((error) => {
+      console.error(error)
+    })
 }
 </script>
+
+<script></script>
--- src/views/SpotShowPage.vue
+++ src/views/SpotShowPage.vue
@@ -1,20 +1,135 @@
 <template>
-  <div>
-    <main-header />
-    <h1>投稿詳細</h1>
-  </div>
+  <v-card class="pa-2" elevation="16" min-width="800" width="100%" height="600px">
+    <v-card-title class="mt-2 mb-4 text-body-3"> 投稿詳細</v-card-title>
+    <v-row justify="end" class="mr-3">
+      <v-btn color="orange" @click="redirectSpotEdit">編集</v-btn>
+    </v-row>
+    <div>
+      平均おすすめ度：{{ spot.average_rating }}
+      <br />
+      平均混雑度：{{ spot.average_quiet_rating }}
+    </div>
+
+    <v-card-item>
+      <div>スポット名：{{ spot.name }}</div>
+      <div>説明：{{ spot.body }}</div>
+      <div>都道府県：{{ spot.prefecture.name }}</div>
+      <div>市町村・番地{{ spot.city }}</div>
+      <br />
+      <div>おすすめ度: {{ assessment.rating }}</div>
+      <div>混雑の少なさ: {{ assessment.quiet_rating }}</div>
+    </v-card-item>
+
+    <v-row class="mt-4" no-gutters justify="center">
+      <v-col cols="12">
+        <v-select
+          :label="'おすすめ度'"
+          v-model="assessment.rating"
+          variant="outlined"
+          density="compact"
+          item-title="name"
+          item-value="id"
+          :items="ratings"
+        />
+      </v-col>
+      <v-col cols="12">
+        <v-select
+          :label="'混雑の少なさ'"
+          v-model="assessment.quiet_rating"
+          variant="outlined"
+          density="compact"
+          item-title="name"
+          item-value="id"
+          :items="ratings"
+        />
+      </v-col>
+
+      <v-btn color="orange" @click="createAssessment">送信</v-btn>
+    </v-row>
+  </v-card>
 </template>
 
 <script setup>
- // import { ref, computed } from 'vue'
-</script>
+import { ref, computed, onMounted } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import axios from '../plugins/axios'
 
-<script>
-import MainHeader from './MainHeader.vue'
+onMounted(() => {
+  fetchSpot()
+})
 
-export default {
-  components: {
-    MainHeader
+const route = useRoute()
+const router = useRouter()
+const ratings = ref([1, 2, 3, 4, 5])
+
+const spot = ref({
+  name: null,
+  body: null,
+  city: null,
+  prefecture: {
+    name: null
   }
+})
+const assessment = ref({
+  rating: null,
+  quiet_rating: null
+})
+
+const createAssessment = () => {
+  const token = localStorage.getItem('access-token')
+  const client = localStorage.getItem('client')
+  const uid = localStorage.getItem('uid')
+  const headers = {
+    'access-token': token,
+    uid: uid,
+    client: client
+  }
+
+  const params = {
+    rating: assessment.value.rating,
+    quiet_rating: assessment.value.quiet_rating
+  }
+
+  axios
+    .post(`api/v1/spots/${spot.value.id}/assessments`, params, {
+      headers
+    })
+    .then((res) => {
+      assessment.value.rating = null
+      assessment.value.quiet_rating = null
+      spot.value = res.data.spot
+      alert('評価しました。')
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+}
+
+const redirectSpotEdit = () => {
+  console.log(route)
+  router.push({ name: 'spot_edit', params: { id: route.params.id } })
+}
+
+const fetchSpot = () => {
+  const token = localStorage.getItem('access-token')
+  const client = localStorage.getItem('client')
+  const uid = localStorage.getItem('uid')
+  const headers = {
+    'access-token': token,
+    uid: uid,
+    client: client
+  }
+
+  const id = route.params.id
+  axios
+    .get(`api/v1/spots/${id}`, { headers })
+    .then((res) => {
+      spot.value = res.data.spot
+    })
+    .catch((error) => {
+      console.error(error)
+    })
 }
 </script>
+
+<script></script>
