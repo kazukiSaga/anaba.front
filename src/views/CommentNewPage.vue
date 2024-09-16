e83c502a5060b2f5897aabb706789ab5acfd3398
kazukiSaga <kazukikk1351@gmail.com>
2024-09-17 03:36:17 +0900

    投稿作成

--- src/views/CommentNewPage.vue
+++ src/views/CommentNewPage.vue
@@ -1,20 +1,11 @@
 <template>
   <div>
-    <main-header />
     <h1>コメント投稿</h1>
   </div>
 </template>
 
 <script setup>
- // import { ref, computed } from 'vue'
+// import { ref, computed } from 'vue'
 </script>
 
-<script>
-import MainHeader from './MainHeader.vue'
-
-export default {
-  components: {
-    MainHeader
-  }
-}
-</script>
+<script></script>
