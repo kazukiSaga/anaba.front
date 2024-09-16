bbb4fc1e6f71d9a962515b29ce7af9c1e4d86508
kazukiSaga <kazukikk1351@gmail.com>
2024-09-17 03:36:17 +0900

    投稿作成

--- src/views/SpotNewPage.vue
+++ src/views/SpotNewPage.vue
@@ -1,107 +1,107 @@
 <template>
-  <div class="spot-post-page">
-    <main-header />
-    <div class="spot-post-container">
-      <h1>スポット投稿</h1>
-      <div class="form-group">
-        <label for="spot-title">タイトル</label>
-        <input type="text" id="spot-title" placeholder="スポットのタイトルを入力" />
-      </div>
+  <div>
+    <v-card elevation="16" min-width="800" width="100%" height="600px">
+      <template v-if="errors.length">
+        <v-alert
+          density="compact"
+          :text="errors.join(', ')"
+          name="入力に誤りがあります"
+          type="warning"
+        ></v-alert>
+      </template>
 
-      <div class="form-group">
-        <label for="spot-description">説明</label>
-        <textarea id="spot-description" rows="5" placeholder="スポットの詳細を入力"></textarea>
-      </div>
+      <v-card-item>
+        <v-card-title class="mt-2 mb-4 text-body-3"> スポット投稿</v-card-title>
+        <v-text-field :label="'タイトル'" v-model="name" variant="outlined" density="compact" />
+        <v-textarea :label="'説明'" v-model="body" variant="outlined" density="compact" />
+        <v-select
+          :label="'都道府県'"
+          v-model="prefecture_id"
+          variant="outlined"
+          density="compact"
+          item-title="name"
+          item-value="id"
+          :items="prefectures"
+        />
+        <v-text-field v-model="city" :label="'市町村・番地'" variant="outlined" density="compact" />
+        <!-- <label for="spot-tags">タグ</label> -->
+        <!-- <input type="text" id="spot-tags" placeholder="タグを入力 (カンマ区切り)" /> -->
+      </v-card-item>
 
-      <div class="form-group">
-        <label for="spot-prefecture">都道府県</label>
-        <input type="text" id="spot-prefecture" placeholder="都道府県を入力" />
-      </div>
-
-      <div class="form-group">
-        <label for="spot-address">市町村・番地</label>
-        <input type="text" id="spot-address" placeholder="市町村・番地を入力" />
-      </div>
-
-      <div class="form-group">
-        <label for="spot-tags">タグ</label>
-        <input type="text" id="spot-tags" placeholder="タグを入力 (カンマ区切り)" />
-      </div>
-
-      <button type="button" class="post-button">投稿する</button>
-    </div>
+      <v-row class="mt-4" no-gutters justify="center">
+        <v-btn color="orange" @click="post">投稿する</v-btn>
+      </v-row>
+    </v-card>
   </div>
 </template>
 
-<script>
-import MainHeader from './MainHeader.vue'
+<script setup>
+import { ref, computed, onMounted } from 'vue'
+import axios from '../plugins/axios'
+import { errorMessages } from 'vue/compiler-sfc'
+import { useRoute, useRouter } from 'vue-router'
 
-export default {
-  components: {
-    MainHeader
+onMounted(() => {
+  fetchSpot()
+})
+
+const prefectures = ref([])
+const errors = ref([])
+const name = ref(null)
+const body = ref(null)
+const prefecture_id = ref(null)
+const city = ref(null)
+const router = useRouter()
+
+const fetchSpot = () => {
+  const token = localStorage.getItem('access-token')
+  const client = localStorage.getItem('client')
+  const uid = localStorage.getItem('uid')
+  const headers = {
+    'access-token': token,
+    uid: uid,
+    client: client
   }
+  axios
+    .get('/api/v1/spots/new', { headers })
+    .then((res) => {
+      console.log(res)
+      prefectures.value = res.data.prefectures
+      errors.value = []
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+}
+
+const post = () => {
+  const token = localStorage.getItem('access-token')
+  const client = localStorage.getItem('client')
+  const uid = localStorage.getItem('uid')
+  const headers = {
+    'access-token': token,
+    uid: uid,
+    client: client
+  }
+
+  const params = {
+    name: name.value,
+    body: body.value,
+    prefecture_id: prefecture_id.value,
+    city: city.value
+  }
+  axios
+    .post('/api/v1/spots', params, { headers })
+    .then((res) => {
+      console.log(res)
+      alert('投稿しました。')
+      router.push({ name: 'spot_show', params: { id: res.data.spot.id } })
+    })
+    .catch((error) => {
+      console.error(error)
+      errors.value = error.response.data.errors
+    })
 }
 </script>
 
-<style scoped>
-.spot-post-page {
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
-  min-height: 100vh;
-  padding: 100px 20px;
-  background-color: white;
-}
-
-.spot-post-container {
-  width: 100%;
-  max-width: 1200px;
-  padding: 40px;
-  background-color: white;
-  border-radius: 10px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-}
-
-h1 {
-  font-size: 36px;
-  margin-bottom: 30px;
-  color: #333;
-  text-align: center;
-}
-
-.form-group {
-  margin-bottom: 25px;
-}
-
-.form-group label {
-  display: block;
-  font-size: 20px;
-  margin-bottom: 10px;
-}
-
-.form-group input,
-.form-group textarea {
-  width: 100%;
-  padding: 20px;
-  font-size: 18px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-}
-
-.post-button {
-  margin-top: 30px;
-  padding: 15px 30px;
-  font-size: 20px;
-  background-color: #87CEEB;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-}
-
-.post-button:hover {
-  background-color: #00BFFF;
-}
-</style>
-
+<style scoped></style>
