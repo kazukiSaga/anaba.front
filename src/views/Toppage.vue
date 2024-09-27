14b2089d81a74f1bd9eb034fa8f7de619f4f22ff
kazukiSaga <kazukikk1351@gmail.com>
2024-09-27 23:47:49 +0900

    google map apiキーを環境変数に入れる

--- src/views/Toppage.vue
+++ src/views/Toppage.vue
@@ -1,29 +1,52 @@
+<!-- template 見た目定義 -->
 <template>
-  <div class="full-background">
-    <div class="content-background">
-      <div class="toppage">
-        <div class="text-container">
-          <h1><span class="big-A">A</span>naba</h1>
-          <ul>
-            <li>
-              <router-link to="/spots">穴場スポット一覧</router-link>
-            </li>
-            <li>
-              <router-link to="/map">穴場スポットMAP</router-link>
-            </li>
-          </ul>
-        </div>
-        <div class="logo">
-          <img src="@/assets/camera8.svg" alt="Logo" class="logo-image" />
+  <div>
+    <div class="full-background">
+      <div class="content-background">
+        <div class="toppage">
+          <div class="text-container" style="margin-right: 50px">
+            <h1><span class="big-A">A</span>naba</h1>
+            <ul>
+              <li>
+                <router-link to="/spots">穴場スポット一覧</router-link>
+              </li>
+              <li>
+                <router-link to="/map">穴場スポットMAP</router-link>
+              </li>
+            </ul>
+          </div>
+          <div class="logo" style="margin-left: 50px">
+            <img src="@/assets/camera8.svg" alt="Logo" class="logo-image" />
+          </div>
         </div>
       </div>
+    </div>
+    <div class="map-container">
+      <GoogleMap
+        class="google-map"
+        :api-key="apiKey"
+        style="width: 100%; height: 800px"
+        :center="center"
+        :zoom="15"
+      >
+        <Marker :options="{ position: center }" />
+      </GoogleMap>
     </div>
   </div>
 </template>
 
-<script></script>
+<script setup>
+// script ロジック、動き、データ定義
+
+import { GoogleMap, Marker } from 'vue3-google-map'
+
+const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+const center = { lat: 35.65856, lng: 139.745461 }
+</script>
 
 <style scoped>
+/* デザイン定義 色や大きさ */
+
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap');
 
 /* 背景は白のまま */
@@ -41,7 +64,6 @@
   justify-content: space-between;
   align-items: flex-start;
   width: 100%;
-  padding-right: 50px;
 }
 
 .text-container {
@@ -76,7 +98,7 @@
 
 .text-container ul li {
   color: black;
-  margin: 0px 0;
+  margin: 0 0;
   font-size: 50px;
 }
 
@@ -85,14 +107,19 @@
   text-decoration: none;
 }
 
-.logo {
-  position: absolute;
-  right: 250px;
-  margin-top: 60px;
-}
-
 .logo-image {
   width: 400px;
   height: auto;
+}
+
+.map-container {
+  width: 100%; /* 横幅を全体に広げる */
+  justify-content: center;
+}
+.google-map {
+  margin-top: 5px;
+  margin-bottom: 100px;
+  width: 100%;
+  max-width: 1200px;
 }
 </style>
