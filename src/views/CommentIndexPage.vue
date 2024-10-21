b18fdc7779c83369580582e3d183d3988a838600
kazukiSaga <kazukikk1351@gmail.com>
2024-10-22 01:49:51 +0900

    コメント一覧実装

--- src/views/CommentIndexPage.vue
+++ src/views/CommentIndexPage.vue
@@ -5,7 +5,36 @@
 </template>
 
 <script setup>
-// import { ref, computed } from 'vue'
-</script>
+import { ref, computed, onMounted } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import axios from '../plugins/axios'
 
-<script></script>
+const route = useRoute()
+
+const commnets = ref([])
+
+onMounted(() => {
+  fetchComments()
+})
+
+const fetchComments = () => {
+  const token = localStorage.getItem('access-token')
+  const client = localStorage.getItem('client')
+  const uid = localStorage.getItem('uid')
+  const headers = {
+    'access-token': token,
+    uid: uid,
+    client: client
+  }
+
+  const spot_id = route.params.id
+  axios
+    .get(`api/v1/spots/${spot_id}/comments`, { headers })
+    .then((res) => {
+      commnets.value = res.data.commnets
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+}
+</script>
