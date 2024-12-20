83c5ced5fca85398978f6c843f24034666f5bfb4
kazukiSaga <kazukikk1351@gmail.com>
2024-12-20 19:11:48 +0900

    vue3-google-map設定

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    rollupOptions: {
+      external: ['vue3-google-map']
+    }
   }
 })
