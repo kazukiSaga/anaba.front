76a0aad2d82cb11e0babe86598e8821b1aa0cfc2
kazukiSaga <kazukikk1351@gmail.com>
2025-01-02 10:39:05 +0900

    認証トークン設定

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -4,7 +4,22 @@ const apiUrl = import.meta.env.VITE_API_URL
 // const apiUrl = 'https://anaba.onrender.com'
 
 const instance = axios.create({
-  baseURL: apiUrl
-})
+  baseURL: apiUrl,
+  headers: {
+    'Content-Type': 'application/json'
+  },
+});
+
+// リクエスト時に認証トークン付加
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('authToken'); // ローカルストレージからトークンを取得
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`; // Authorizationヘッダーを設定
+  }
+  return config;
+}, (error) => {
+  // リクエスト時にエラーがあった場合
+  return Promise.reject(error);
+});
 
 export default instance
